4a007a5b458c00e6a0d625b45b4d30026013411a
문석환 <tjrghks96@naver.com>
2023-08-29 15:11:02 +0900

    Global Font 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,10 @@
 import './globals.css';
 
 import type { Metadata } from 'next';
-import { Open_Sans } from 'next/font/google';
 
 import Footer from '@/components/layout/Footer';
 import Header from '@/components/layout/Header';
-
-const sans = Open_Sans({ subsets: ['latin'] });
+import { fontSans } from '@/lib/fonts';
 
 export const metadata: Metadata = {
   title: {
@@ -21,7 +19,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className={sans.className}>
+    <html lang="en" className={fontSans.className}>
       <body className="bg-primary">
         <div className="mx-auto flex h-full w-full max-w-3xl flex-col px-6 md:max-w-7xl">
           <Header />
