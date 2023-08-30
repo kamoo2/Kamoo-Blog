0c8763265fd063966cadcdf8f2fd1ec876a9959a
문석환 <tjrghks96@naver.com>
2023-08-31 01:03:42 +0900

    Update : IconText.tsx props 추가

--- src/app/components/common/IconText.tsx
+++ src/app/components/common/IconText.tsx
@@ -1,15 +1,26 @@
 export type IconTextProps = {
   // eslint-disable-next-line unused-imports/no-unused-vars
-  Icon: (props: React.ComponentProps<'svg'>) => JSX.Element;
+  Icon?: (props: React.ComponentProps<'svg'>) => JSX.Element;
   text?: React.ReactNode;
+  subText?: React.ReactNode;
   className?: string;
+  textStyle?: string;
+  subTextStyle?: string;
 };
 
-export default function IconText({ Icon, text, className }: IconTextProps) {
+export default function IconText({
+  Icon,
+  text,
+  subText,
+  className,
+  textStyle,
+  subTextStyle,
+}: IconTextProps) {
   return (
     <div className={`flex items-center ${className ?? 'gap-1 text-xs'}`}>
-      <Icon />
-      <span>{text}</span>
+      {Icon && <Icon />}
+      <span className={textStyle}>{text}</span>
+      <span className={subTextStyle}>{subText}</span>
     </div>
   );
 }
