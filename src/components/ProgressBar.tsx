b2e34691b302ecbfd9e1b9c8e57d833043168989
문석환 <tjrghks96@naver.com>
2023-09-07 18:35:37 +0900

    Feat : Post 페이지 ProgressBar 구현

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -1,6 +1,6 @@
 'use client';
-import { useEffect, useState } from 'react';
 import { usePathname } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
 export default function ProgressBar() {
   const [width, setWidth] = useState(0);
