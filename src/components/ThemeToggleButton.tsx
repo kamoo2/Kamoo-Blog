d0b7452ded67f9e698374fa539fbe4639f27274f
문석환 <tjrghks96@naver.com>
2023-09-17 17:21:57 +0900

    Fix : Mounted 전 theme값 undefined로 인식되어 발생하는 문제 해결

--- src/components/ThemeToggleButton.tsx
+++ src/components/ThemeToggleButton.tsx
@@ -1,17 +1,24 @@
 'use client';
 
-import { HiMiniSun, HiMoon } from 'react-icons/hi2';
+import { useEffect, useState } from 'react';
+import { HiMiniMoon, HiMiniSun } from 'react-icons/hi2';
 
 import IconButton from '@/components/common/IconButton';
 import useSwitchTheme from '@/components/useSwitchTheme';
 
 export default function ThemeToggleButton() {
   const { theme, toggleTheme } = useSwitchTheme();
+  const [mounted, setMounted] = useState(false);
 
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return <div className="h-[36px] w-[36px]"></div>;
   return (
     <IconButton
       onClick={toggleTheme}
-      Icon={theme === 'dark' ? HiMoon : HiMiniSun}
+      Icon={theme === 'dark' ? HiMiniMoon : HiMiniSun}
       className="rounded-sm p-2 text-xl text-amber-300 transition-colors duration-300 hover:bg-neutral-150 dark:text-amber-200 dark:hover:bg-neutral-800"
     />
   );
