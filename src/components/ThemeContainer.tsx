d73f6f52121da870a56f8114079fac84900bee66
문석환 <tjrghks96@naver.com>
2023-09-17 16:30:23 +0900

    Fix : 초기에 페이지 접속시 Dark Mode로 나오는 문제

--- src/components/ThemeContainer.tsx
+++ src/components/ThemeContainer.tsx
@@ -3,5 +3,14 @@ import { ThemeProvider } from 'next-themes';
 import { ReactNode } from 'react';
 
 export default function ThemeContainer({ children }: { children: ReactNode }) {
-  return <ThemeProvider attribute="class">{children}</ThemeProvider>;
+  return (
+    <ThemeProvider
+      storageKey="theme"
+      attribute="class"
+      defaultTheme="light"
+      themes={['light', 'dark']}
+    >
+      {children}
+    </ThemeProvider>
+  );
 }
